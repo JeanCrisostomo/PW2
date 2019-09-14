c4c82d5f2dfbd46260c830465aa01bd56d0846a8
Jean <jean.campos.v@gmail.com>
2019-09-14 17:40:21 -0300

    Alterar comparacao Strings CalcBean Class

--- src/edu/ifrs/CalcBean.java
+++ src/edu/ifrs/CalcBean.java
@@ -9,19 +9,19 @@ public class CalcBean {
 		
 	}
 	
-	public int calculate(int valor1, int valor2, String operacao) {
-			int resposta=0;
-			
-			if(operacao=="+") {
-	        	resposta = valor1 + valor2;
-	        } if(operacao=="-") {
-	        	resposta = valor1 - valor2;
-	        } if(operacao=="x") {
-	        	resposta = valor1 * valor2;
-	        } if (operacao == "/") {
-	        	resposta = valor1 / valor2;
-	        }
-			
-			return resposta;
-		}
+	public int mathOp(int valor1, int valor2, String operacao) {
+		int resposta=0;
+		
+		if("+".equals(operacao)) {
+        	resposta = valor1 + valor2;
+        }else if("-".equals(operacao)) {
+        	resposta = valor1 - valor2;
+        }else if("x".equals(operacao)) {
+        	resposta = valor1 * valor2;
+        }else {
+        	resposta = valor1 / valor2;
+        }
+		
+		return resposta;
+	}
 }
